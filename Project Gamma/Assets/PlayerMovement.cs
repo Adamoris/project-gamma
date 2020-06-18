a30133ef6e68e2079236915a11111b7bc1a4b6b5
Kyle Liston <sillysmithishere@gmail.com>
2020-06-18 02:07:22 -0700

    Movement Version 1
    Basic movement (right click)

---
+++ Project Gamma/Assets/PlayerMovement.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+	public Camera cam;
+	public Rigidbody2D rb;
+    // Update is called once per frame
+    void Update()
+    {
+		rb.MovePosition(cam.ScreenToWorldPoint(Input.mousePosition));
+    }
+}
---
+++ Project Gamma/Assets/TestingMove.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using UnityEngine;
+
+public class TestingMove : MonoBehaviour
+{
+    public float speed;
+	public float accel;
+	public Camera cam;
+	public Rigidbody2D rb;
+	public Vector2 dir;
+	Vector2 goTo;
+	Vector2 velocity;
+    // Start is called before the first frame update
+    void Start()
+    {
+		
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		if(Input.GetMouseButtonDown(1)) {
+			goTo = cam.ScreenToWorldPoint(Input.mousePosition);
+		}
+		dir = new Vector2(goTo.x-rb.position.x, goTo.y-rb.position.y);
+		if(Vector2.Distance(rb.position, goTo) > 0.1) {
+			velocity.x += Math.Sign(dir.x-velocity.x)*accel*Math.Abs(dir.x);
+			velocity.y += Math.Sign(dir.y-velocity.y)*accel*Math.Abs(dir.y);
+			if(velocity.magnitude > speed) {
+				velocity.x = velocity.x/1.5f;
+				velocity.y = velocity.y/1.5f;
+			}
+			rb.MovePosition(rb.position + velocity * Time.fixedDeltaTime);
+		}
+    }
+}
