343729cd969c738b7a216cfa37d90c23070b94bf
Andrew Park <joohyung122002@gmail.com>
2020-06-21 15:40:00 -0700

    Minimap System Implemented
    It should now be easy to create a minimap for future iterations.

---
+++ Project Gamma/Assets/Scripts/UI/Minimap/MinimapClass.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Minimap
+{
+    public static class MinimapClass
+    {
+
+        public static event EventHandler OnWindowShow;
+        public static event EventHandler OnWindowHide;
+        public static event EventHandler OnZoomChanged;
+
+        public static void Init()
+        {
+            MinimapCamera.OnZoomChanged += MinimapCamera_OnZoomChanged;
+            MinimapWindow.OnWindowShow += MinimapWindow_OnWindowShow;
+            MinimapWindow.OnWindowHide += MinimapWindow_OnWindowHide;
+        }
+
+        private static void MinimapCamera_OnZoomChanged(object sender, EventArgs e)
+        {
+            OnZoomChanged?.Invoke(sender, e);
+        }
+
+        private static void MinimapWindow_OnWindowShow(object sender, EventArgs e)
+        {
+            OnWindowShow?.Invoke(sender, e);
+        }
+
+        private static void MinimapWindow_OnWindowHide(object sender, EventArgs e)
+        {
+            OnWindowHide?.Invoke(sender, e);
+        }
+
+        public static void ShowWindow()
+        {
+            MinimapWindow.Show();
+        }
+
+        public static void HideWindow()
+        {
+            MinimapWindow.Hide();
+        }
+
+        public static void SetZoom(float orthographicSize)
+        {
+            MinimapCamera.SetZoom(orthographicSize);
+        }
+
+        public static float GetZoom()
+        {
+            return MinimapCamera.GetZoom();
+        }
+
+        public static void ZoomIn()
+        {
+            MinimapCamera.ZoomIn();
+        }
+
+        public static void ZoomOut()
+        {
+            MinimapCamera.ZoomOut();
+        }
+    }
+}
+
