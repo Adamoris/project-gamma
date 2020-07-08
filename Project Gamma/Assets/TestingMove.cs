f830058b65942c33a5a123048d47163390430c25
Kyle Liston <sillysmithishere@gmail.com>
2020-07-07 20:05:05 -0700

    Movement V4
    Accel/Decel, rotation, collison

--- Project Gamma/Assets/TestingMove.cs
+++ Project Gamma/Assets/TestingMove.cs
@@ -8,20 +8,22 @@ public class TestingMove : MonoBehaviour
 {
     public float speed;
 	public float accel;
+	public float turnSpeed;
+	float currentspeed = 0;
 	public Camera cam;
 	public Rigidbody2D rb;
 	public Vector2 dir;
 	List<Vector2> waypoints = new List<Vector2>();
 	Vector2 velocity;
 	Vector2 goTo;
+	float total;
     // Start is called before the first frame update
     void Start()
     {
 		cam = FindObjectOfType<Camera>();
     }
-    // Update is called once per frame
-    void Update()
-    {
+	
+	void Update() {
 		if(Input.GetMouseButtonDown(1)) {
 			if(Input.GetKey("left shift")) {
 				waypoints.Add(cam.ScreenToWorldPoint(Input.mousePosition));
@@ -30,23 +32,42 @@ public class TestingMove : MonoBehaviour
 				waypoints.Add(cam.ScreenToWorldPoint(Input.mousePosition));
 			}
 		}
+	}
+	
+    void FixedUpdate()
+    {
+		total = 0;
+		for(int i=0;i<waypoints.Count;i++) {
+			if(i == 0) {
+				total += Vector2.Distance(waypoints[i], rb.position);
+			} else {
+				total += Vector2.Distance(waypoints[i], waypoints[i-1]);
+			}
+		}
+		if(currentspeed != total) {
+			currentspeed += Math.Sign(total-currentspeed)*accel;
+		}
+		if(currentspeed < 0f) {
+			currentspeed = 0f;
+		} else if(currentspeed > speed) {
+			currentspeed = speed;
+		}
 		goTo = waypoints.Count > 0 ? waypoints[0] : goTo;
 		dir = new Vector2(goTo.x-rb.position.x, goTo.y-rb.position.y);
-		if(Vector2.Distance(rb.position, goTo) > 0.1) {
-			velocity.x += dir.x;
-			velocity.y += dir.y;
-			//velocity = dir;
-			if(velocity.magnitude > speed) {
-				velocity.Normalize();
-				velocity *= speed;
-			}
-			rb.MovePosition(rb.position + velocity * Time.fixedDeltaTime);
-		} else if( waypoints.Count > 0) {
+		if(Vector2.Distance(rb.position, goTo) > 0.2) {
+			velocity = dir;
+			velocity.Normalize();
+			velocity *= currentspeed;
+			rb.MovePosition(rb.position + velocity * Time.deltaTime);
+		} else if(waypoints.Count > 0) {
 			waypoints.RemoveAt(0);
 		} else {
 			velocity.x = 0f;
 			velocity.y = 0f;
 		}
-		transform.rotation = Quaternion.LookRotation(Vector3.forward, velocity);
+		
+		if(velocity.magnitude != 0) {
+			transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.LookRotation(Vector3.forward, velocity), Time.deltaTime * turnSpeed);
+		}
     }
 }
